1f514b5f404efa5314cf0ca48062db4a63daf2e6
serge cleoron <sc21417@gmail.com>
2019-08-01 18:01:58 +0200

    Automated commit at 2019-08-01 16:01:58 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/navshrink-3088ae80.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(document).scrollTop()>80?($(".navbar-lrhg").css("height",70),$(".navbar-lrhg-brand img").css("height",50),$(".dropdown-toggle").css("font-size",20)):($(".navbar-lrhg").css("height",120),$(".navbar-lrhg-brand img").css("height",90),$(".dropdown-toggle").css("font-size",30))});
