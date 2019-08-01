2fc87bb6fe0d151cd8fe65e1652a345d1c760f3a
serge cleoron <sc21417@gmail.com>
2019-08-01 17:02:02 +0200

    clear javascript

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
 
-  $(".navbar-lrhg-item").hover(
-    function(){ $(".banner-content h2").addClass('reveal') },
-    function(){ $(".banner-content h2").removeClass('reveal') }
-  )
-
 /*Smooth the animation to get the anchor tag links*/
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
